4a5d3b7db65f9bdd3a3eeb2d4d544a8516019900
claudealdric <cvaldric@gmail.com>
2022-06-24 01:40:12 -0400

    Create user model/GQL object

---
+++ src/users/user.model.ts
@@ -0,0 +1,13 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  firstName: string;
+
+  @Field({ nullable: true })
+  lastName: string;
+}
