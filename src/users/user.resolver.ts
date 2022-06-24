ac7dbbc34161086da1399d1ef4d5b68a60c67f85
claudealdric <cvaldric@gmail.com>
2022-06-24 02:52:42 -0400

    Add `getUserById` functionality

--- src/users/user.resolver.ts
+++ src/users/user.resolver.ts
@@ -1,10 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { User } from './user.model';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => User, { name: 'user' })
+  async getUserById(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<User> {
+    return this.usersService.getUserById(id);
+  }
 
   @Query(() => [User], { name: 'users' })
   async getAllUsers(): Promise<User[]> {
