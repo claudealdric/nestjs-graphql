6a3b064bf3e9a79ce876e29dac84669e2b71f134
claudealdric <cvaldric@gmail.com>
2022-06-25 19:01:47 -0400

    Refactor

--- src/users/dto/user.input.ts
+++ src/users/dto/user.input.ts
@@ -1,10 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
-import { BaseEntity } from 'src/shared/base.entity';
 import { User } from '../user.entity';
 
 @InputType()
-export class UserInput implements Omit<User, 'id' | keyof BaseEntity> {
+export class UserInput implements Pick<User, 'firstName' | 'lastName'> {
   @Field()
   @IsNotEmpty()
   @IsString()
