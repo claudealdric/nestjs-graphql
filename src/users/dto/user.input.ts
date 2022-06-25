0aaa5d69400e43b03a6581b7cba8a606b4eb2335
claudealdric <cvaldric@gmail.com>
2022-06-25 16:47:03 -0400

    Implement `BaseEntity` in all relevant areas

--- src/users/dto/user.input.ts
+++ src/users/dto/user.input.ts
@@ -1,9 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { BaseEntity } from 'src/shared/base.entity';
 import { User } from '../user.entity';
 
 @InputType()
-export class UserInput implements Omit<User, 'id'> {
+export class UserInput implements Omit<User, 'id' | keyof BaseEntity> {
   @Field()
   @IsNotEmpty()
   @IsString()
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,8 @@
+import { BaseEntity } from '../shared/base.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
