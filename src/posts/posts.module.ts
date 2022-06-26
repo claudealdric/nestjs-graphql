d01d157a7f3ce0f6b52c86da6d5f5fa258cea232
claudealdric <cvaldric@gmail.com>
2022-06-26 02:41:31 -0400

    Add posts seeding functionality

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
 import { Post } from './post.entity';
 import { PostsResolver } from './post.resolver';
 import { PostsService } from './posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post, User])],
   providers: [PostsService, PostsResolver],
   exports: [PostsService],
 })
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,19 +1,53 @@
+import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 import { Post } from './post.entity';
 
 @Injectable()
 export class PostsService {
   constructor(
-    @InjectRepository(Post) private readonly repository: Repository<Post>,
+    @InjectRepository(Post) private readonly postsRepository: Repository<Post>,
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
 
-  getPosts(): Promise<Post[]> {
-    return this.repository.find();
+  async getPosts(): Promise<Post[]> {
+    let posts = await this.postsRepository.find();
+    if (posts.length === 0) {
+      await this.seedPosts();
+      posts = await this.postsRepository.find();
+    }
+    return posts;
   }
 
   getPostsForUser(userId: number): Promise<Post[]> {
-    return this.repository.find({ where: { userId: userId } });
+    return this.postsRepository.find({ where: { userId: userId } });
+  }
+
+  private async seedPosts(): Promise<void> {
+    let userIds = (await this.usersRepository.find({ select: ['id'] })).map(
+      (user) => user.id,
+    );
+    userIds = Array.from(new Set(userIds));
+    const posts: Post[] = [];
+
+    for (const userId of userIds) {
+      const numberOfPostsToCreate = Math.floor(Math.random() * 3);
+      for (let i = 0; i < numberOfPostsToCreate; i++) {
+        const newPost = this.createFakePostForUser(userId);
+        posts.push(newPost);
+      }
+    }
+
+    await this.postsRepository.save(posts);
+  }
+
+  private createFakePostForUser(userId: number): Post {
+    const post = this.postsRepository.create();
+    post.title = faker.lorem.words();
+    post.body = faker.lorem.paragraph(1);
+    post.userId = userId;
+    return post;
   }
 }
