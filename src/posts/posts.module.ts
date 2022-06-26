e2037e3f9394447fa376591ea2d56a5f1d23358d
claudealdric <cvaldric@gmail.com>
2022-06-26 13:43:47 -0400

    Rename files

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
 import { Post } from './post.entity';
-import { PostsResolver } from './post.resolver';
+import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
 
 @Module({
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from '../posts/posts.module';
 import { User } from './user.entity';
-import { UsersResolver } from './user.resolver';
+import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
